98ad69b0f7d93da7b25b1f8ab6097630d728c5a0
DavideStenner <48441753+DavideStenner@users.noreply.github.com>
2025-04-04 10:30:09 +0000

    eval all score script

---
+++ eval_score.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the list of datasets
+datasets=("adult" "default" "shoppers" "magic" "beijing" "news")
+
+# Define the list of methods
+models=("smote" "stasy" "codi" "tabddpm" "tabsyn" "real")
+
+# Iterate over each dataset
+for dataset in "${datasets[@]}"; do
+    # Iterate over each method for each dataset
+    for model in "${models[@]}"; do
+        echo "Evaluating MLE $model for dataset: $dataset"
+        python eval/eval_mle.py --dataname "$dataset" --model "$model"
+
+        echo "Evaluating Detection $model for dataset: $dataset"
+        python eval/eval_detection.py --dataname "$dataset" --model "$model"
+
+    done
+done
+
+echo "All training and sampling processes are completed."
